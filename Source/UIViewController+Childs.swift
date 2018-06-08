dd45d92be86bb5dfd4b10d3d5338901d2ad774c0
SiSo Mollov <mollov@me.com>
2018-06-08 14:23:52 +0300

    add option to set a containerView in add(_ child) method

--- Source/UIViewController+Childs.swift
+++ Source/UIViewController+Childs.swift
@@ -1,8 +1,9 @@
 import UIKit
 
 extension UIViewController {
-  public func add(_ child: UIViewController) {
+  public func add(_ child: UIViewController, containerView: UIView? = nil) {
     addChildViewController(child)
+    let view: UIView = containerView ?? self.view
     view.addSubview(child.view)
     child.didMove(toParentViewController: self)
   }
